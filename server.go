ca15abe38982a9699c948462b5c9971016a1efb9
Kamil Zak <kamilzak00@gmail.com>
2022-04-06 20:03:39 +0100

    feat: added basic k:v add/read for badger

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
+	"github.com/Pallinder/go-randomdata"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/django"
 )
@@ -15,6 +19,26 @@ func main() {
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		opts := badger.DefaultOptions("/tmp/badger")
+		opts.Logger = nil
+		db, err := badger.Open(opts)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Inserting 100,000 records.")
+		start := time.Now()
+
+		for n := 0; n < 100000; n++ {
+			log.Printf("Item: %d", n)
+			setTransaction(db)
+		}
+
+		log.Printf("Completed in %v", time.Since(start))
+
+		defer db.Close()
+
 		return c.Render("index", fiber.Map{
 			"title": "Hello, World!",
 		})
@@ -22,4 +46,48 @@ func main() {
 
 	app.Static("/", "./views")
 	log.Fatal(app.Listen(":3000"))
+}
+
+func setTransaction(db *badger.DB) error {
+	txn := db.NewTransaction(true)
+	defer txn.Discard()
+
+	err := txn.Set([]byte(randomdata.SillyName()), []byte(randomdata.SillyName()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := txn.Commit(); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func getTransaction(db *badger.DB) error {
+	err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("path"))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = item.Value(func(val []byte) error {
+			fmt.Printf("%s\n", val)
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
